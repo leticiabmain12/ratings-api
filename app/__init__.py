fa962bf03c6f08ec632873a23a60b96d8ff5f091
Ada <ada@Adas-MBP.hitronhub.home>
2022-02-08 20:52:30 -0800

    changing the postgress url

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,7 @@ def create_app(test_config=None):
     app = Flask(__name__)
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
   
-    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://postgres:postgres@localhost:5432/brand_ratings"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://postgres:postgres@https://fast-mountain-02468.herokuapp.com"
 
     db.init_app(app)
     migrate.init_app(app,db)
